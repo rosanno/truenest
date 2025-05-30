72daba7accad263d727e979360dba6d922f4aa4c
rosanno <rosannomacabuhay@gmail.com>
2025-05-30 21:16:36 +0800

    add custom error in authjs authentication

--- features/login/components/login-form.tsx
+++ features/login/components/login-form.tsx
@@ -4,9 +4,10 @@ import React, { useState } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { Loader, LockKeyhole } from "lucide-react";
+import { Loader, LockKeyhole, Eye, EyeOff } from "lucide-react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
@@ -22,6 +23,7 @@ const formSchema = z.object({
 
 const LoginForm = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -45,7 +47,12 @@ const LoginForm = () => {
 
     console.log(result);
 
-    router.push("/dashboard");
+    if (!result?.error) {
+      router.push("/dashboard");
+    } else {
+      toast.error(result?.code);
+    }
+
     setIsLoading(false);
   };
 
@@ -84,7 +91,12 @@ const LoginForm = () => {
                   <FormItem>
                     <FormLabel>Password</FormLabel>
                     <FormControl>
-                      <Input {...field} type="password" placeholder="Enter your password" disabled={isLoading} />
+                      <div className="relative">
+                        <Input {...field} type={showPassword ? "text" : "password"} placeholder="Enter your password" disabled={isLoading} />
+                        <button type="button" onClick={() => setShowPassword(!showPassword)} className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700">
+                          {!showPassword ? <EyeOff className="h-4 w-4" /> : <Eye className="h-4 w-4" />}
+                        </button>
+                      </div>
                     </FormControl>
                     <FormMessage />
                   </FormItem>
